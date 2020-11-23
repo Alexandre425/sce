627f0b0b6d70067ffe9a963480a9241055211dd9
Alexandre425 <alexandre.d.rodrigues@tecnico.ulisboa.pt>
2020-11-23 20:50:53 +0000

    finished terminal command comments

--- remote_terminal/monitor.c
+++ remote_terminal/monitor.c
@@ -44,11 +44,9 @@ extern void cmd_rmh(int, char **);
 extern void cmd_test(int, char **);
 void cmd_help(int, char **);
 
-/*-------------------------------------------------------------------------+
-| Variable and constants definition
-+--------------------------------------------------------------------------*/
-const char TitleMsg[] = "\n Application Control Monitor (tst)\n";
-const char InvalMsg[] = "\nInvalid command!";
+
+const char TitleMsg[] = "\n\n";
+const char InvalMsg[] = "\tInvalid command!\n";
 
 struct command_d
 {
@@ -66,27 +64,27 @@ struct command_d
 	{cmd_test, 	"teste", "	<arg1> <arg2>  	comando de teste"},
 
 	{cmd_comm_read_clock, 				"rc", "					read the clock"},
-	{cmd_comm_set_clock, 				"sc", "		<h><m><s>	set the clock"},
+	{cmd_comm_set_clock, 				"sc", "		<h> <m> <s>	set the clock"},
 	{cmd_comm_read_temp_lum, 			"rtl", "				read temperature and luminosity"},
 	{cmd_comm_read_param, 				"rp", "					read the PMON and TALA parameters"},
 	{cmd_comm_mod_monitor_period, 		"mmp", "	<p>			modify the monitoring period (0 - disable)"},
 	{cmd_comm_mod_time_alarm, 			"mta", "	<s>			modify the time the alarm is active"},
 	{cmd_comm_read_alarms, 				"ra", "					read the alarms and alarm enable"},
-	{cmd_comm_define_alarm_clock, 		"dac", ""},
-	{cmd_comm_define_temp_lum, 			"dtl", ""},
-	{cmd_comm_activate_alarms, 			"aa", ""},
-	{cmd_comm_info_reg, 				"ir", ""},
-	{cmd_comm_transfer_reg, 			"trc", ""},
-	{cmd_comm_transfer_reg_from, 		"tri", ""},
-	{cmd_local_info_local_reg, 			"irl", ""},
-	{cmd_local_list_reg, 				"lr", ""},
-	{cmd_local_delete_reg, 				"dr", ""},
-	{cmd_proc_check_period_transfer, 	"cpt", ""},
-	{cmd_proc_mod_period_tranfer, 		"mpt", ""},
-	{cmd_proc_check_thresh_temp_lum, 	"cttl", ""},
-	{cmd_proc_define_thresh_temp_lum, 	"dttl", ""},
-	{cmd_proc_process_reg, 				"pr", ""}
-	};
+	{cmd_comm_define_alarm_clock, 		"dac", "	<h> <m> <s>	modify the time of the alarm clock"},
+	{cmd_comm_define_temp_lum, 			"dtl", "	<t> <l>		modify the temperature and luminosity thresholds"},
+	{cmd_comm_activate_alarms, 			"aa", "		<a>			enable/disable the alarms (1/0)"},
+	{cmd_comm_info_reg, 				"ir", "					print information about the registers"},
+	{cmd_comm_transfer_reg, 			"trc", "	<n>			transfer n registers from the current index"},
+	{cmd_comm_transfer_reg_from, 		"tri", "	<n> <i>		transfer n registers from the index i"},
+	{cmd_local_info_local_reg, 			"irl", "				print information about the local registers"},
+	{cmd_local_list_reg, 				"lr", "		<n> <i>		list n local memory registers from index i"},
+	{cmd_local_delete_reg, 				"dr", "					delete all local registers"},
+	{cmd_proc_check_period_transfer, 	"cpt", "				print the period of transfer of registers"},
+	{cmd_proc_mod_period_tranfer, 		"mpt", "	<p>			modify the period of transfer (minutes, 0 - disable"},
+	{cmd_proc_check_thresh_temp_lum, 	"cttl", "				print the processing temperature and luminosity threshold"},
+	{cmd_proc_define_thresh_temp_lum, 	"dttl", "				modify the processing temperature and luminosity thresholds"},
+	{cmd_proc_process_reg, 				"pr", "		<t1> <t2>	process the registers between the t1 and t2 (hh mm ss) instants"}
+};
 
 #define NCOMMANDS (sizeof(commands) / sizeof(struct command_d))
 #define ARGVECSIZE 10
@@ -137,7 +135,8 @@ void monitor(void)
 	static char *argv[ARGVECSIZE + 1], *p;
 	int argc, i;
 
-	printf("%s Type help for help\n", TitleMsg);
+	printf(TitleMsg);
+	printf("Type \"help\" for a detailed command list\n");
 	for (;;)
 	{
 		printf("\nCmd> ");
