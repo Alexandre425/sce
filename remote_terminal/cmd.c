6e57b7215f474d9352757abded6b093594091c56
Alexandre425 <alexandre.d.rodrigues@tecnico.ulisboa.pt>
2020-11-30 16:13:39 +0000

    started all 3 threads

--- remote_terminal/cmd.c
+++ remote_terminal/cmd.c
@@ -18,9 +18,19 @@ static cyg_thread comm_send_thread, comm_recv_thread, proc_thread;
 extern void cmd_ini(int, char **);
 extern void monitor(void);
 
-static void test (cyg_addrword_t data)
+static void comm_send_entry (cyg_addrword_t data)
 {
-	printf("New thread!\n");
+	printf("Communication (send) thread initialized!\n");
+}
+
+static void comm_recv_entry (cyg_addrword_t data)
+{
+	printf("Communication (receive) thread initialized!\n");
+}
+
+static void proc_entry (cyg_addrword_t data)
+{
+	printf("Processing thread initialized!\n");
 }
 
 int main(void)
@@ -31,10 +41,21 @@ int main(void)
 	// Creating the 3 other threads
 	// Communication (send) thread
 	cyg_thread_create(
-		COMM_SEND_PRI, &test, 0, "test", comm_send_stack, 
+		COMM_SEND_PRI, &comm_send_entry, 0, "comm_send", comm_send_stack, 
 		STACK_SIZE, &comm_send_handle, &comm_send_thread
 	);
+	// Communication (receive) thread
+	cyg_thread_create(
+		COMM_RECV_PRI, &comm_recv_entry, 0, "comm_recv", comm_recv_stack, 
+		STACK_SIZE, &comm_recv_handle, &comm_recv_thread
+	);
+	cyg_thread_create(
+		PROC_PRI, &proc_entry, 0, "proc", proc_stack, 
+		STACK_SIZE, &proc_handle, &proc_thread
+	);
 	cyg_thread_resume(comm_send_handle);
+	cyg_thread_resume(comm_recv_handle);
+	cyg_thread_resume(proc_handle);
 
 	monitor();
 
