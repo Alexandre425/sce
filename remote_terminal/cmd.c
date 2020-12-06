428acc3bbb16f43eb59b306a127ac7991998dcce
Alexandre425 <alexandre.d.rodrigues@tecnico.ulisboa.pt>
2020-12-06 02:19:47 +0000

    additional data test

--- remote_terminal/cmd.c
+++ remote_terminal/cmd.c
@@ -2,7 +2,6 @@
 #include <cyg/kernel/kapi.h>
 #include <cyg/hal/hal_arch.h>
 
-
 #define COMM_SEND_PRI 2
 #define COMM_RECV_PRI 1
 #define PROC_PRI 3
@@ -12,8 +11,18 @@ static unsigned char comm_send_stack[STACK_SIZE];
 static unsigned char comm_recv_stack[STACK_SIZE]; 
 static unsigned char proc_stack[STACK_SIZE];
 
+// Handles for the threads
 static cyg_handle_t comm_send_handle, comm_recv_handle, proc_handle;
 static cyg_thread comm_send_thread, comm_recv_thread, proc_thread;
+
+// Variables to store the mailboxes
+static cyg_mbox comm_mbox, proc_mbox;
+
+typedef struct common_info
+{
+	// Mailbox handles
+	cyg_handle_t comm_mbox_handle, proc_mbox_handle;
+} common_info_t;
 
 extern void cmd_ini(int, char **);
 extern void monitor(void);
@@ -21,6 +30,7 @@ extern void monitor(void);
 static void comm_send_entry (cyg_addrword_t data)
 {
 	printf("Communication (send) thread initialized!\n");
+	printf("Received pointer to data %x\n", (void*)data);
 }
 
 static void comm_recv_entry (cyg_addrword_t data)
@@ -38,10 +48,16 @@ int main(void)
 	// Doing IO lookup
 	cmd_ini(0, NULL);
 
+	// Generating the common information struct
+	common_info_t com_info; 
+	// Creating the mailboxes
+	cyg_mbox_create(&com_info.comm_mbox_handle, &comm_mbox);
+	cyg_mbox_create(&com_info.proc_mbox_handle, &proc_mbox);
+
 	// Creating the 3 other threads
 	// Communication (send) thread
 	cyg_thread_create(
-		COMM_SEND_PRI, &comm_send_entry, 0, "comm_send", comm_send_stack, 
+		COMM_SEND_PRI, &comm_send_entry, &com_info, "comm_send", comm_send_stack, 
 		STACK_SIZE, &comm_send_handle, &comm_send_thread
 	);
 	// Communication (receive) thread
